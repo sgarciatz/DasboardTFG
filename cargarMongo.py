f430d8c62bdf660d9e5664a383484fa9187a98df
sgarciatz <sgarciatz@alumnos.unex.es>
2022-06-14 16:16:01 +0000

    Adecuacion de scripts de carga a la mv

--- cargarMongo.py
+++ cargarMongo.py
@@ -14,13 +14,14 @@ fs            = gridfs.GridFS(client.fotosAgro, "fotosRGB")
 for image in rgb_images:
     with open(rgb_folder + "/" + image, "rb") as image_file:
         try:
-            image_timestamp = datetime.fromisoformat(imagen[:26])
+            image_timestamp = datetime.fromisoformat(image[:26])
             cam_pos         = int(image[33])
             cam_id          = int(image[41])
             fs.put(image_file, filename=image, metadata={"timestamp": image_timestamp, "pos": cam_pos, "idCam": cam_id})
-        except:
-            print("Se ha intentado insertar una imagen ya existente")
-        print(imagen[:26], imagen[33], imagen[41], "\n")
+            print(f"Imagen insertada -> {image_timestamp} || {cam_pos} || {cam_id}")
+        except Exception as e:
+            print(f"Se ha intentado insertar una imagen ya existente {image}")
+            print(e)
 
 fs = gridfs.GridFS(client.fotosAgro, "fotosHyper")
 
@@ -29,8 +30,8 @@ for image in hyper_images:
         try:
             image_timestamp = datetime.fromisoformat(image[5:31])
             fs.put(image_file, filename=image, metadata={"timestamp": image_timestamp})
+            print(f"Imagen HIM insertada -> {image_timestamp}")
         except:
-            print("Se ha intendao insertar una imagen ya existente")
-        print(imagen, "\n")
-
+            print(f"Se ha intendao insertar una imagen ya existente {image}")
+            print(e)
 client.close()
--- updateSharedFolder.sh
+++ updateSharedFolder.sh
@@ -1,9 +1,9 @@
 if [ $# -lt 3 ]; then
-    printf "Se debe ejecutar este script con 3 parametros -> updateSharedFolder.sh <usuario> <contrasena> <urlRemoto>\n"
+    printf "Se debe ejecutar este script con 3 parametros -> updateSharedFolder.sh <usuario> <contrasena> <urlRemoto> <excludeList>\n"
     exit
 fi
 
-shared_folder=$(pwd)/Documents/University/TFG/DashboardTFG/sharedFolder
+shared_folder=$HOME/DashboardTFG/sharedFolder
 
 printf "${shared_folder}\n"
 
@@ -11,8 +11,8 @@ if [ ! -d  $shared_folder ]; then
     mkdir $shared_folder
 fi
 
-printf "nextcloudcmd --silent --user ${1} --password ${2} ${sharedFolder} ${3}\n"
+printf "nextcloudcmd --silent --exclude $4 --user ${1} --password ${2} ${sharedFolder} ${3} \n"
 
-nextcloudcmd --user $1 --password $2 $shared_folder $3
+nextcloudcmd --silent --exclude $4 --user $1 --password $2 $shared_folder $3
 
-/bin/python3.8 /home/santi/Documents/University/TFG/DashboardTFG/cargarMongo.py
+python3.10 $HOME/DashboardTFG/cargarMongo.py
